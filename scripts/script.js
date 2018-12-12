0900b8b61edc913317458e7994ec5a43e59a29a2
TEKORA-C\ochernyshenko <ochernyshenko@pmpractice.ru>
2018-12-12 17:23:53 +0300

    Это первый коммит

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+ var number = 5;
+ var string = "Hello";
+ var sym = Symbol();
+ var boolean = true;
+ null;
+ undefined;
+ var obj = {};
+
+// console.log(4/0);
+ //console.log('string'*9);
+
+ //let something;
+ //console.log(something);
+
+ let persone = {
+     name: "John",
+     age: 25,
+     isMarried: false
+ }
+
+ //console.log(persone.name);
+
+ //let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+ //onsole.log(arr[2]);
+
+ //let answer = confirm("Are you ready?");
+ // console.log(answer);
+
+ //let answer = +prompt("Есть ли вам 18?", "Да");
+// console.log(typeof(answer));
+
+ //console.log(4 + +"- object");
+
+ let incr = 10,
+     decr = 10;
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log("2" == 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || !isClose);
